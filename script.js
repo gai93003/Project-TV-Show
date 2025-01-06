98fafb0ceaa2f9f49cf6ceaef3e2378eb2ddbe7a
gai93003 <deng93003@gmail.com>
2025-01-06 22:24:18 +0000

    I created an anchor element to nest the episode names

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function makePageForEpisodes(episodeList) {
     const formattedseason = String(episodeItem.season).padStart(2, '0');
     const forEpisode = String(episodeItem.number).padStart(2, '0');
     const h3 = card.querySelector('h3').textContent = `${episodeItem.name} ~ S${formattedseason}E${forEpisode}`;
+    const link = card.querySelector('a');
+    link.href = episodeItem.url
 
     console.log(formattedseason, forEpisode)
     // section.append(h3);
