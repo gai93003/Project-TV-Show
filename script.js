d1214a52e486d674cae2cc3d6af76d5aa061048e
gai93003 <deng93003@gmail.com>
2025-01-11 16:13:34 +0000

    I have finished this part of the project

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
+let statusFlag = "loading";
+let allEpisodes = [];
+
+async function setup() {
+    await getAllEpisodes();
+  
+  makePageForEpisodes(allEpisodes);
+  
+}
+
+async function getAllEpisodes() {
+  try {
+    const response = await fetch("https://api.tvmaze.com/shows/82/episodes");
+    const data = await response.json();
+
+    allEpisodes = data;
+    statusFlag = "success";
+  } catch {
+    statusFlag = "error";
+  }
+
   searchEpisodes();
   selectEpisode();
   makePageForEpisodes(allEpisodes);
 }
 
-const inputEl = document.getElementById("inputEl");
-
 function searchEpisodes() {
   inputEl.addEventListener("input", () => {
     const searchTerm = inputEl.value.toLowerCase();
-    console.log(inputEl.value);
-    const allEpisodes = getAllEpisodes();
-    //compare value to real data episode names and summary
+    if (searchTerm !== '') {
+      document.getElementById('select-el').value = 'all';
+    }
+    const selected = document.getElementById('selected');
     const filteredEpisodes = allEpisodes.filter((episode) => {
       return (
         episode.name.toLowerCase().includes(searchTerm) ||
@@ -25,39 +43,43 @@ function searchEpisodes() {
   });
 }
 
-
-
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("all-episodes");
   rootElem.innerHTML = "";
-  for(const episodeItem of episodeList) {
-    const card = document.getElementById("film-card").content.cloneNode(true);
-    const section = card.querySelector("section");
-    const image = (card.querySelector("img").src = episodeItem.image.medium);
-    const summary = (card.querySelector("#summary").innerHTML =
-      episodeItem.summary);
 
-    const formattedseason = String(episodeItem.season).padStart(2, "0");
-    const forEpisode = String(episodeItem.number).padStart(2, "0");
-    const h3 = (card.querySelector(
-      "h3"
-    ).textContent = `${episodeItem.name} ~ S${formattedseason}E${forEpisode}`);
-    const link = card.querySelector("a");
-    link.href = episodeItem.url;
+    if(statusFlag === 'loading') {
+      rootElem.innerHTML = '<p>Please wait, loading data...</p>'
+    }
+    else if (statusFlag === 'success') {
+      for (const episodeItem of episodeList) {
+        const card = document.getElementById("film-card").content.cloneNode(true);
+        const section = card.querySelector("section");
+        const image = (card.querySelector("img").src = episodeItem.image.medium);
+        const summary = (card.querySelector("#summary").innerHTML =
+          episodeItem.summary);
+    
+        const formattedseason = String(episodeItem.season).padStart(2, "0");
+        const forEpisode = String(episodeItem.number).padStart(2, "0");
+        const h3 = (card.querySelector(
+          "h3"
+        ).textContent = `${episodeItem.name} ~ S${formattedseason}E${forEpisode}`);
+        const link = card.querySelector("a");
+        link.href = episodeItem.url;
+    
+        card.appendChild(section);
+        rootElem.append(card);
+      }         
+    }
+    else {
+      rootElem.innerHTML = '<p>Oops, something went wrong. Please try again.</p>'
+    }
 
-    //console.log(formattedseason, forEpisode)
-    // section.append(h3);
-    card.appendChild(section);
-    rootElem.append(card);
-  };
 }
+
 function selectEpisode() {
   const selectEl = document.getElementById("select-el");
-  const allEpisodes = getAllEpisodes();
 
-  allEpisodes.forEach (episode =>{
-
-   
+  allEpisodes.forEach((episode) => {
     const optionEl = document.createElement("option");
 
     const formattedseason = String(episode.season).padStart(2, "0");
@@ -65,23 +87,25 @@ function selectEpisode() {
     optionEl.textContent = `S${formattedseason}E${forEpisode} - ${episode.name}`;
     optionEl.value = episode.id;
     selectEl.appendChild(optionEl);
-  })
-  selectEl.addEventListener("change", (ev) =>{
-    if(ev.target.value === "all"){
-      makePageForEpisodes(allEpisodes)
-    } else{
-      const pickedEpisode = allEpisodes.find(epsd => epsd.id == ev.target.value
-      )
-      makePageForEpisodes([pickedEpisode])
+  });
+  selectEl.addEventListener("change", (ev) => {
+    document.getElementById('inputEl').value = '';
+    if (ev.target.value === "all") {
+      makePageForEpisodes(allEpisodes);
+    } else {
+      const pickedEpisode = allEpisodes.find(
+        (epsd) => epsd.id == ev.target.value
+      );
+      makePageForEpisodes([pickedEpisode]);
+      displayEpisode(allEpisodes.length, 1);
+
     }
-  })
+  });
 }
 
-function displayEpisode(allEpisodes,pickedEpisode) {
+function displayEpisode(allEpisodes, pickedEpisode) {
   const paragraghEl = document.getElementById("display");
-  paragraghEl.textContent = `Display ${pickedEpisode}/${allEpisodes}`
-  
-  
+  paragraghEl.textContent = `Display ${pickedEpisode}/${allEpisodes}`;
 }
-searchEpisodes();
+
 window.onload = setup;
